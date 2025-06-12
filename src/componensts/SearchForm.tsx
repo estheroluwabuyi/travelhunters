c057e330dc0d9d444b87659c196cc75eb0f85d6f
Esther Okiki <estherokiki15@gmail.com>
2025-06-12 17:39:07 +0100

    fix text

--- src/componensts/SearchForm.tsx
+++ src/componensts/SearchForm.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function SearchForm() {
   return (
-    <form className='bg-white w-full rounded-3xl text-black'>
+    <form className='bg-white w-full rounded-3xl text-black text-[1.4rem]'>
 fficia distinctio, soluta itaque nostrum neque laudantium molestias atque ea expedita excepturi vitae numquam? Tenetur assumenda quae voluptatem esse?
     </form>
   )
